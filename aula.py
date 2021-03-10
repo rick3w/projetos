c18b6b47bbf3a8dece2d9a70667dc85c775e6b4a
Jonathan Cardoso <jjcc.rick3w@gmail.com>
2021-03-09 23:03:17 -0300

    chute um numero

--- aula.py
+++ aula.py
@@ -1,3 +1,27 @@
-print('Olá, Mundo!')
-print('Claudyana linda!')
-csdcsdc
+from random import randint
+
+class ChuteONumero:
+    def __init__(self):
+        self.chute = 0
+        self.aleatorio = 0
+
+    def Iniciar(self):
+        self.GerarNumAleatorio()
+        while True:
+            self.NumChute()
+            if self.chute > self.aleatorio:
+                print('Chute um valor menor')
+            elif self.chute < self.aleatorio:
+                print('Chute um valor maior')
+            elif self.chute == self.aleatorio:
+                print('\nPARABÉNS! Acertou na mosca')
+                break
+    
+    def GerarNumAleatorio(self):
+        self.aleatorio = randint(1, 100)
+
+    def NumChute(self):
+        self.chute = int(input('\nChute um número: '))
+
+chute = ChuteONumero()
+chute.Iniciar()
