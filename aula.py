c9856dabcfd494dc588f01cbf7349ddbc532dc4f
Jonathan Cardoso <jjcc.rick3w@gmail.com>
2021-03-10 17:18:28 -0300

    ate q enfim consegui fazer esse validador! ufa ;D

--- aula.py
+++ aula.py
@@ -2,6 +2,26 @@ cpf = input('Digite o cpf: ')
 novo_cpf = ''
 
 for d in cpf:
-    if d.isdigit():
+    if d.isdigit() and len(novo_cpf) < 9:
         novo_cpf += d
 
+cont_1 = 10
+cont_2 = 11
+total_1 = 0
+total_2 = 0
+
+for d in novo_cpf:
+    total_1 += int(d) * cont_1
+    cont_1 -= 1
+
+digito_10 = total_1 * 10 % 11
+novo_cpf += str(digito_10)
+
+for d in novo_cpf:
+    total_2 += int(d) * cont_2
+    cont_2 -= 1
+
+digito_11 = total_2 * 10 % 11
+novo_cpf += str(digito_11)
+
+print(novo_cpf)
