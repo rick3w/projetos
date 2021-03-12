b3420ad8bf1479f8d20bcaaa39e50874b06bdd38
Jonathan Cardoso <jjcc.rick3w@gmail.com>
2021-03-12 08:56:13 -0300

    validador de cpf simples

--- validador_cpf.py
+++ validador_cpf.py
@@ -1,30 +1,42 @@
-cpf = input('Digite o cpf: ')
-novo_cpf = ''
+class ValidadorCPF:
+    def __init__(self):
+        self.cpf = input('\nDigite o cpf: ')
+        self.cpf2 = ''
 
-for d in cpf[:-2]:
-    if d.isdigit():
-        novo_cpf += d
+    def novo_cpf(self):
+        for d in self.cpf[:-2]:
+            if d.isdigit():
+                self.cpf2 += d
 
-cont_1 = 10
-cont_2 = 11
-total_1 = 0
-total_2 = 0
+    def digito_10(self):
+        cont = 10
+        total = 0
+        for d in self.cpf2:
+            total += int(d) * cont
+            cont -= 1
+        dig_10 = total * 10 % 11
+        self.cpf2 += str(dig_10)
 
-for d in novo_cpf:
-    total_1 += int(d) * cont_1
-    cont_1 -= 1
+    def digito_11(self):
+        cont = 11
+        total = 0
+        for d in self.cpf2:
+            total += int(d) * cont
+            cont -= 1
+        dig_11 = total * 10 % 11
+        self.cpf2 += str(dig_11)
 
-digito_10 = total_1 * 10 % 11
-novo_cpf += str(digito_10)
+    def validador(self):
+        self.novo_cpf()
+        self.digito_10()
+        self.digito_11()
 
-for d in novo_cpf:
-    total_2 += int(d) * cont_2
-    cont_2 -= 1
+    def resultado(self):
+        self.validador()
+        if self.cpf == self.cpf2:
+            print('O cpf é VÁLIDO!')
+        else:
+            print('O cpf é INVÁLIDO!')
 
-digito_11 = total_2 * 10 % 11
-novo_cpf += str(digito_11)
-
-if cpf == novo_cpf:
-    print('O cpf é VÁLIDO!')
-else:
-    print('O cpf é INVÁLIDO!')
+cpf = ValidadorCPF()
+cpf.resultado()
