d152cf43176b5d338edbb8d514ad1e69de47e330
Jonathan Cardoso <jjcc.rick3w@gmail.com>
2021-03-25 17:05:07 -0300

    venv

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-def titulo(titulo, tamanho):
-    print()
-    print('-' * tamanho)
-    print(f'{titulo:^{tamanho}}')
-    print('-' * tamanho)
-
-titulo('TECH N GAMES', 40)
-
-print('\nSEJA BEM VINDO!\n')
+print('olá, mundo!')
